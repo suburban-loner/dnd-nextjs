679fed64a57eadb841c6f3a151b4448c9a17f3bb
suburban-loner <trent8423@gmail.com>
2025-01-10 16:23:23 +0200

    fixed layout and got rid of dashboard folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,15 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { SearchForm } from "@/components/search-form";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +36,28 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <SidebarProvider>
+          <AppSidebar />
+          <SidebarInset>
+            <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+              <div className="flex items-center gap-2 px-3">
+                <SidebarTrigger />
+              </div>
+              <div className="grow h-4" />
+              <SearchForm />
+              <div className="flex pr-4">
+                <Button asChild variant="link" className="p-2">
+                  <Link href="/login">Login</Link>
+                </Button>
+
+                <Button asChild variant="link" className="p-2">
+                  <Link href="/signup">Sign Up</Link>
+                </Button>
+              </div>
+            </header>
+            <main className="flex flex-1 flex-col gap-4 p-4">{children}</main>
+          </SidebarInset>
+        </SidebarProvider>
       </body>
     </html>
   );
--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,9 +1,10 @@
-import * as React from "react"
-import { GalleryVerticalEnd } from "lucide-react"
-import Link from "next/link"
+import * as React from "react";
+import Link from "next/link";
+import Image from "next/image";
 import {
   Sidebar,
   SidebarContent,
+  SidebarFooter,
   SidebarGroup,
   SidebarHeader,
   SidebarMenu,
@@ -13,7 +14,7 @@ import {
   SidebarMenuSubButton,
   SidebarMenuSubItem,
   SidebarRail,
-} from "@/components/ui/sidebar"
+} from "@/components/ui/sidebar";
 
 const data = {
   navMain: [
@@ -63,7 +64,7 @@ const data = {
       ],
     },
   ],
-}
+};
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
@@ -71,17 +72,17 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
       <SidebarHeader>
         <SidebarMenu>
           <SidebarMenuItem>
-            <SidebarMenuButton size="lg" asChild>
-              <Link href="#">
-                <div className="flex aspect-square  items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
-                  <GalleryVerticalEnd className="size-4" />
-                </div>
-                <div className="flex flex-col gap-0.5 leading-none">
-                  <span className="font-semibold">Documentation</span>
-                  <span className="">v1.0.0</span>
-                </div>
-              </Link>
-            </SidebarMenuButton>
+            <Link href="#">
+              <div className="flex items-center justify-center">
+                <Image
+                  src="/Logo-dnd.jpg"
+                  alt="Logo for the dnd campaign."
+                  width={100}
+                  height={100}
+                  className="rounded-lg"
+                />
+              </div>
+            </Link>
           </SidebarMenuItem>
         </SidebarMenu>
       </SidebarHeader>
@@ -111,7 +112,15 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
           </SidebarMenu>
         </SidebarGroup>
       </SidebarContent>
+      <SidebarFooter>
+        <Link href="/Help" passHref>
+          <button>Help</button>
+        </Link>
+        <Link href="/Contact-form">
+          <button>Contact</button>
+        </Link>
+      </SidebarFooter>
       <SidebarRail />
     </Sidebar>
-  )
+  );
 }
